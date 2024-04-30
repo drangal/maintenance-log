e0e04b538e872ab87db16759015229533925d27e
drangal <danik.kis03@mail.ru>
2024-05-01 01:54:56 +0300

    change text on protected page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import DeployButton from '../components/DeployButton'
 import AuthButton from '../components/AuthButton'
 import { createClient } from '@/utils/supabase/server'
-import ConnectSupabaseSteps from '@/components/tutorial/ConnectSupabaseSteps'
-import SignUpUserSteps from '@/components/tutorial/SignUpUserSteps'
 import Header from '@/components/Header'
 import LogLogo from '@/components/LogLogo'
 
